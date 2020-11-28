3cf50961aa7ab94996dfc1b0a98973bb6fdfc900
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-11-28 03:35:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function inOrder(currNode)
+{
+   if(currNode.left){
+    inOrder(currNode.left)
+  }
+  console.log(currNode.data)
+  if(currNode.right){
+    inOrder(currNode.right)
+  }
+}
+
+function findOrAdd(node, num)
+{
+  until()
+}
+
+function max()
+{
+  
+}
+
+function min()
+{
+  
+}
